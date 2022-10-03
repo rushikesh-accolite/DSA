967eb8844c80131cb17472352cd66aff7ce7c67a
Rushikesh Ghotekar <106075304+rushikesh-accolite@users.noreply.github.com>
2022-10-03 11:01:23 +0530

    Update CountSort.java

--- DSAjava/src/main/java/org/dsa/CountSort.java
+++ DSAjava/src/main/java/org/dsa/CountSort.java
@@ -1,3 +1,7 @@
+/*Given an array of integers, sort the array according to frequency of elements. 
+That is elements that have higher frequency come first. 
+If frequencies of two elements are same, then smaller number comes first. */
+
 package org.dsa;
 
 import java.util.*;
@@ -11,31 +15,16 @@ public class CountSort {
                 list.add(a[i]);
             }
             for(int i=0;i<n;i++){
-                mp.put(list.get(i),mp.getOrDefault(list.get(i),0)+1);
+                mp.put(list.get(i),mp.getOrDefault(list.get(i),0)+1); //Store Frequency of all elements of the Array
             }
-            Collections.sort(list, (n1, n2) -> {
+            Collections.sort(list, (n1, n2) -> {                //Custom Comparator to sort the Array
                 int f1 = mp.get(n1);
                 int f2 = mp.get(n2);
 
-                if (f1 != f2) return f2-f1;
-                return n1 - n2;
+                if (f1 != f2) return f2-f1;             //Compare Frequencies
+                return n1 - n2;                         //If frequencies are same sort by their value
             });
-            return list;
-        }
-
-        public static void main(String[] args) {
-            Scanner x = new Scanner(System.in);
-            int t = x.nextInt();   //testcases
-            for (int i = 0; i < t; i++) {
-                int n = x.nextInt();
-                int a[] = new int[n];
-                for (int j = 0; j < n; j++) {
-                    a[j] = x.nextInt();
-                }
-                List<Integer>ans=fn(a, n);
-                System.out.println(ans);
-            }
-
+            return list;                //Return the Answer
         }
 
 }
